d4f12952afcc7d02d9d027a2842763ef0d5da660
danielpickett <daniel@danielpickett.me>
2021-06-22 00:16:54 -0400

    I did this

---
+++ script.js
@@ -0,0 +1,53 @@
+const input = document.getElementById('input')
+const output = document.getElementById('output')
+
+const sampleCode = `; Some brackets in a comment that will be ignored )]}{[(
+(write-line "Some ))( parens in a string }}} literal for  \\"No ))( Reason\\"")
+
+(defun csg-intersection-intersect-all (obj-a obj-b)
+(lambda (ray)
+  (flet ((inside-p (obj) (lambda (d) (inside-p obj (ray-point ray d)))))
+    (merge 'fvector
+           (remove-if-not (inside-p obj-b) (intersect-all obj-a ray))
+           (remove-if-not (inside-p obj-a) (intersect-all obj-b ray))
+           #'<))))`
+
+const hasBalancedBrackets = (str) => {
+  str = str.replace(/;.*/, '') // strip LISP comments
+  const stack = []
+  const map = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+  }
+
+  let isInQuote = false
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i]
+    if (char === '"' && str[i - 1] !== '\\') isInQuote = !isInQuote
+    if (char in map && !isInQuote) stack.push(char)
+    else if (Object.values(map).indexOf(char) > -1 && !isInQuote) {
+      if (char === map[stack.slice(-1)]) stack.pop()
+      else return false
+    }
+  }
+
+  if (stack.length !== 0) return false
+
+  return true
+}
+
+const updateValue = () => {
+  const valid = hasBalancedBrackets(input.value)
+  output.innerText = valid
+    ? '✌️ Piece and happiness ✌️'
+    : '🔥 WHOA!! Bad code alert! 🔥'
+  if (valid) output.classList.remove('invalid')
+  else output.classList.add('invalid')
+}
+
+input.value = sampleCode
+updateValue()
+
+window.addEventListener('input', updateValue)
